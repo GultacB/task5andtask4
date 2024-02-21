0c18cc71e322dab80a5437412346f2a8dbc4b671
GultacB <160045911+GultacB@users.noreply.github.com>
2024-02-21 15:54:56 +0400

    task4andtask5

---
+++ task4.1/task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Secdiyiniz menyu elementini daxil edin:");
+string menu =Console.ReadLine();
+switch (menu)
+{  
+    case "Play":
+       Console.WriteLine(" Sizi artiq Play interface qarsilayir ");
+       break;
+    case "Replay":
+        Console.WriteLine("Sizi artiq Replay interface qarsilayir");
+        break;
+    case "Next":
+         Console.WriteLine("Sizi artiq Next interface qarsilayir");
+         break;
+    case "Back":
+          Console.WriteLine("Sizi artiq Back interface qarsilayir");
+        break;
+    
+}  
